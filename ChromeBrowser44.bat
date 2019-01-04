90691e849a9d9963744e081c00b1cb99d01ce39f
youmoxiang <1406611607@qq.com>
2019-01-04 21:35:23 +0800

    试用提交部分小文件 fangk

---
+++ ChromeBrowser44.bat
@@ -0,0 +1,2 @@
+7-Zip\7z e SmartPiEMS\data\Web.zip -o%Temp%\Chrome44 Widgets\PiNVR\VideoReview\Chrome44.0.2403.155_x86.zip
+%WinDir%\explorer %Temp%\Chrome44\Chrome44.0.2403.155_x86.zip
---
+++ ChromeBrowserUpToDate.bat
@@ -0,0 +1,2 @@
+7-Zip\7z e SmartPiEMS\data\Web.zip -o%Temp%\Chrome Contents\Soft\ChromeStandaloneSetup.exe
+%Temp%\Chrome\ChromeStandalone_65.0.3325.181_Setup.exe
